c846ce8e499f343fa228a3511ef9493b51c67732
csouza <carolinasouza092@hotmail.com>
2023-05-26 16:35:53 -0300

    aplicación de js en formulario de contacto para ver datos en consola

--- codigo.js
+++ codigo.js
@@ -1,1 +1,16 @@
-onclick
+var formulario = document.querySelector(".formulario-contacto");
+
+function enviarFormulario(event) {
+    event.preventDefault();
+    
+    var userName = document.querySelector("#userName").value;
+    var correo = document.querySelector("#correo").value;
+    var telefono = document.querySelector("#telefono").value;
+    var mensaje = document.querySelector("#mensaje").value;
+
+    console.log("Nombre y apellido: " + userName)
+    console.log("Correo electrónico: " + correo)
+    console.log("Teléfono: " + telefono)
+    console.log("Consulta: " + mensaje)
+}
+    formulario.addEventListener("submit", enviarFormulario);
