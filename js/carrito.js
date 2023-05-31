3ffc2c1d52b402fc8b2ac7c435a3b00fd2026b7b
csouza <carolinasouza092@hotmail.com>
2023-05-31 13:35:34 -0300

    arreglo de funciones carrito

--- js/carrito.js
+++ js/carrito.js
@@ -8,6 +8,8 @@ const carritoTotal = document.getElementById('carrito-total');
 const carritoCantidad = document.getElementById('carrito-cantidad');
 const detalleCarrito = document.getElementById('detalle-carrito');
 const botonPagar = document.querySelector('.carrito-pagar');
+const carritoDatosContainer = document.getElementById('carrito-datos');
+const carritoGuardado = localStorage.getItem('carrito');
 
 // Función para agregar un producto al carrito
 function contratar(nombre, precio) {
@@ -24,33 +26,29 @@ function mostrarDetalleCarrito() {
 // Función para mostrar el carrito en la interfaz de usuario
 function mostrarCarrito() {
     carritoLista.innerHTML = '';
+
     let total = 0;
-    carrito.forEach((producto) => {
+    for (const producto of carrito) {
         const { nombre, precio } = producto;
         const itemCarrito = document.createElement('li');
-        itemCarrito.innerText = `${nombre} - $${precio}`;
+        itemCarrito.textContent = `${nombre} - $${precio}`;
         carritoLista.appendChild(itemCarrito);
         total += precio;
-    });
-
-    carritoTotal.innerText = `Total: $${total}`;
-    carritoCantidad.innerText = carrito.length.toString();
-
-    if (carrito.length > 0) {
-        detalleCarrito.classList.add('visible');
-        botonPagar.disabled = false; // Habilitar el botón de pagar
-    } else {
-        detalleCarrito.classList.remove('visible');
-        botonPagar.disabled = true; // Deshabilitar el botón de pagar
     }
-    guardarCarritoEnLocalStorage ()
+
+    carritoTotal.textContent = `Total: $${total}`;
+    carritoCantidad.textContent = carrito.length;
+
+    detalleCarrito.classList.toggle('visible', carrito.length > 0);
+    botonPagar.disabled = carrito.length === 0;
+
+    guardarCarritoEnLocalStorage();
 }
 
 // Función botón para vaciar el carrito
 function vaciarCarrito() {
     carrito = [];
     mostrarCarrito();
-    mostrarDetalleCarrito();
 }
 
 // Función botón para realizar el pago
@@ -66,12 +64,8 @@ function guardarCarritoEnLocalStorage() {
 
 // Cargar carrito guardado en localStorage
 function cargarCarritoDesdeLocalStorage() {
-    const carritoGuardado = localStorage.getItem('carrito');
-
-    if (carritoGuardado) {
-        carrito = JSON.parse(carritoGuardado);
-        mostrarCarrito();
-    }
+    carrito = carritoGuardado ? JSON.parse(carritoGuardado) : [];
+    mostrarCarrito();
 }
 
 // Cargar el carrito al cargar la página
@@ -80,34 +74,22 @@ window.addEventListener('load', cargarCarritoDesdeLocalStorage);
 
 // Cargar carrito en página de pago
 function cargarDatosCarrito() {
-    const carritoGuardado = localStorage.getItem('carrito');
+    carritoDatosContainer.innerHTML = '';
 
-    if (carritoGuardado) {
-        const carrito = JSON.parse(carritoGuardado);
-        const carritoDatosContainer = document.getElementById('carrito-datos');
-        const listaProductos = document.createElement('ul');
-
-        carrito.forEach((producto) => {
-            const itemProducto = document.createElement('li');
-            itemProducto.textContent = `${producto.nombre} - $${producto.precio}`;
-            listaProductos.appendChild(itemProducto);
-        });
-
-        const total = document.createElement('p');
-        total.textContent = `Total: $${calcularTotal(carrito)}`;
-        carritoDatosContainer.innerHTML = '';
-        carritoDatosContainer.appendChild(listaProductos);
-        carritoDatosContainer.appendChild(total);
+    for (const producto of carrito) {
+        const itemProducto = document.createElement('li');
+        itemProducto.textContent = `${producto.nombre} - $${producto.precio}`;
+        carritoDatosContainer.appendChild(itemProducto);
     }
+
+    const total = document.createElement('p');
+    total.textContent = `Total: $${calcularTotal()}`;
+    carritoDatosContainer.appendChild(total);
 }
 
 // Calcular el total del carrito
-function calcularTotal(carrito) {
-    let total = 0;
-    carrito.forEach((producto) => {
-        total += producto.precio;
-    });
-    return total;
+function calcularTotal() {
+    return carrito.reduce((total, producto) => total + producto.precio, 0);
 }
 
 // Cargar los datos del carrito al cargar la página de pago
